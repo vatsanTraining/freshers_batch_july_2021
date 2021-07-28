ee55aae3c572507d1ad03b30eca87d4e3bd4e425
srivatsan <vatsank@gmail.com>
2021-07-28 13:46:38 +0530

    Testing with Mockito Added

---
+++ test_doubles_mokito/src/test/java/com/example/demo/mock/TestWithMockitoAnnotation.java
@@ -0,0 +1,57 @@
+package com.example.demo.mock;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import com.example.demo.mocks.Invoice;
+import com.example.demo.mocks.InvoiceRepository;
+import com.example.demo.mocks.InvoiceService;
+
+public class TestWithMockitoAnnotation {
+
+	  @Mock
+	  InvoiceRepository repoMock;
+	 
+	 
+	  @InjectMocks
+	  InvoiceService service ;
+	   
+	  @Before
+	  public void setUp() {
+		   
+		   MockitoAnnotations.initMocks(this);
+	   }
+
+	  @Test
+		@DisplayName("This method verifies with custom created Mock")
+		public void testIvoiceAdd() {
+
+			
+			Invoice inv1 = new Invoice(201,"Naveen", 8000);
+			Invoice inv2 = new Invoice(202,"Vignesh", 6000);
+			
+			  
+			
+			service.addInvoice(inv1);
+			
+			Mockito.verify(repoMock, Mockito.times(1)).add(inv1);
+
+			service.addInvoice(inv2);
+						
+			Mockito.verify(repoMock, Mockito.times(1)).add(inv2);
+
+
+			
+	       
+	           
+	           
+			 
+			  
+		}
+
+}
